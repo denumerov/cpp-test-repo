e844d1d7b8a28c80c61a86444775ed4c78bc2848
denumerov <166035017+denumerov@users.noreply.github.com>
2024-07-09 10:38:59 +0400

    Update iteratorrange_class.cpp

--- iteratorrange_class.cpp
+++ iteratorrange_class.cpp
@@ -1,4 +1,15 @@
-class IteratorRange {
+template <typename Iterator>
+class Paginator {
+    public:
+        Paginator(Iterator begin, Iterator end, int page_size):
+        begin_(begin),
+        end_(end),
+        page_size_(page_size) 
+        {
+            pages_ = SetPages(begin, end, page_size);
+        }
+
+        class IteratorRange {
         public:
         
             IteratorRange(Iterator range_begin, int range_size):
@@ -25,3 +36,33 @@ class IteratorRange {
             Iterator end_;
             int size_;
         };
+
+            auto begin() const {
+                return pages_.begin();
+            }
+
+            auto end() const  {
+                return pages_.end();
+            }
+
+
+    private:
+        Iterator begin_;
+        Iterator end_;
+        int page_size_;
+        vector<IteratorRange> pages_;
+
+        IteratorRange SetPage(Iterator page_begin, int page_size) {
+            return IteratorRange(page_begin, page_size);
+        }
+
+        vector<IteratorRange> SetPages(Iterator begin, Iterator end, int page_size) {
+            vector<IteratorRange> pages;
+            int container_size = distance(begin, end);
+            for (int i = 0; i <= container_size; i + page_size) {
+                    advance(begin, i);
+                    pages.push_back(SetPage(begin, page_size));
+            }
+            return pages;
+        }
+};
